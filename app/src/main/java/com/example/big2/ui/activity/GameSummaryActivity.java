addbf2dd561495e0a986074b2eef774c9912e9d6
luk-ryan <ryankdluk@gmail.com>
2025-04-02 01:47:48 -0400

    change button name to reset, and made alert messages more clear

--- app/src/main/java/com/example/big2/ui/activity/GameSummaryActivity.java
+++ app/src/main/java/com/example/big2/ui/activity/GameSummaryActivity.java
@@ -12,7 +12,6 @@ import android.transition.ChangeBounds;
 import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
@@ -25,7 +24,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
@@ -52,7 +50,7 @@ public class GameSummaryActivity extends AppCompatActivity {
     private TextView tvCardValue, tvTotalP1, tvTotalP2, tvTotalP3, tvTotalP4;
     private TextView tvP1, tvP2, tvP3, tvP4, tvS1, tvS2, tvS3, tvS4;
     private ImageView ivCardValueIcon, ivSuitP1, ivSuitP2, ivSuitP3, ivSuitP4;
-    private Button btnFinishGame, btnRestartGame, btnContinueGame;
+    private Button btnFinishGame, btnResetGame, btnContinueGame;
     private GameViewModel gameViewModel;
     private RoundViewModel roundViewModel;
 
@@ -134,7 +132,7 @@ public class GameSummaryActivity extends AppCompatActivity {
 
         // Buttons
         btnFinishGame = findViewById(R.id.btnFinishGame);
-        btnRestartGame = findViewById(R.id.btnRestartGame);
+        btnResetGame = findViewById(R.id.btnResetGame);
         btnContinueGame = findViewById(R.id.btnContinueGame);
 
         // Observe the LiveData returned by getGameById
@@ -235,10 +233,15 @@ public class GameSummaryActivity extends AppCompatActivity {
         // Back Button - closes activity and sends user back to Gameplay
         ivBack.setOnClickListener(v -> finish());
 
-        ivDelete.setOnClickListener(v -> DialogUtils.showConfirmationDialog(this, "Delete Game", "Are you sure you want to delete this game?", () -> deleteGame(gameId)));
-        btnFinishGame.setOnClickListener(v -> DialogUtils.showConfirmationDialog(this, "Finish Game", "Are you sure you want to finish this game?", () -> finishGame(gameId)));
-        btnRestartGame.setOnClickListener(v -> DialogUtils.showConfirmationDialog(this, "Restart Game", "Are you sure you want to restart this game?", () -> restartGame(gameId)));
-        btnContinueGame.setOnClickListener(v -> DialogUtils.showConfirmationDialog(this, "Continue Game", "Are you sure you want to continue this game?", () -> continueGame(gameId)));
+        String msgDelete = "Are you sure you want to delete this game? Game cannot be retrieved later.";
+        String msgFinish = "Are you sure you want to finish this game? Game will be marked as Completed.";
+        String msgReset = "Are you sure you want to restart this game? Game will start at Round 1 again and all scores will be reset.";
+        String msgContinue = "Are you sure you want to continue this game? Game will start again from the last round that was played.";
+
+        ivDelete.setOnClickListener(v -> DialogUtils.showConfirmationDialog(this, "Delete Game", msgDelete, () -> deleteGame(gameId)));
+        btnFinishGame.setOnClickListener(v -> DialogUtils.showConfirmationDialog(this, "Finish Game", msgFinish, () -> finishGame(gameId)));
+        btnResetGame.setOnClickListener(v -> DialogUtils.showConfirmationDialog(this, "Reset Game", msgReset, () -> restartGame(gameId)));
+        btnContinueGame.setOnClickListener(v -> DialogUtils.showConfirmationDialog(this, "Continue Game", msgContinue, () -> continueGame(gameId)));
     }
 
     // Helper method to format the score
@@ -562,8 +565,8 @@ public class GameSummaryActivity extends AppCompatActivity {
         ConstraintSet constraintSet = new ConstraintSet();
         constraintSet.clone(layout);
 
-        constraintSet.clear(R.id.btnRestartGame, ConstraintSet.START);
-        constraintSet.connect(R.id.btnRestartGame, ConstraintSet.START, referenceButton.getId(), ConstraintSet.END);
+        constraintSet.clear(R.id.btnResetGame, ConstraintSet.START);
+        constraintSet.connect(R.id.btnResetGame, ConstraintSet.START, referenceButton.getId(), ConstraintSet.END);
 
         constraintSet.applyTo(layout);
     }
