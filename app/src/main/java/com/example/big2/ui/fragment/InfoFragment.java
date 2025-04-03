d3f9bec33cb1daa4faf41b267612c9d8c654bb6e
luk-ryan <ryankdluk@gmail.com>
2025-04-03 17:12:34 -0400

    Modified Info Fragment for close easier for user

--- app/src/main/java/com/example/big2/ui/fragment/InfoFragment.java
+++ app/src/main/java/com/example/big2/ui/fragment/InfoFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,6 +12,7 @@ import com.example.big2.R;
 
 public class InfoFragment extends Fragment {
 
+    ImageView ivRuleHands, ivClose;
     public InfoFragment() {
         // Required empty public constructor
     }
@@ -20,8 +21,19 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_info, container, false);
 
-        Button btnClose = view.findViewById(R.id.btnClose);
-        btnClose.setOnClickListener(v -> getParentFragmentManager().popBackStack());
+        ivRuleHands = view.findViewById(R.id.ivRuleHands);
+        ivClose = view.findViewById(R.id.ivClose);
+
+        // Close fragment when clicking outside the ImageView
+        view.setOnClickListener(v -> getParentFragmentManager().popBackStack());
+
+        // Prevent closing when clicking on the image
+        ivRuleHands.setOnClickListener(v -> {
+            // Do nothing to consume the click
+        });
+
+        // Handle close button click
+        ivClose.setOnClickListener(v -> getParentFragmentManager().popBackStack());
 
         return view;
     }
